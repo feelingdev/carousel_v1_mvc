5da28a19e92ca94800c59f9be2df6361c6174db8
marcgrey <49396518+marcgrey@users.noreply.github.com>
2020-11-07 15:55:05 +0800

    Initial Change

---
+++ carousel_v1_mvc/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using carousel_v1_mvc.DataAccess;
+using System.Web.Mvc;
+
+namespace carousel_v1_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var getGames = HomeDataAccess.GetGames();
+            return View(getGames);
+        }
+    }
+}
---
+++ carousel_v1_mvc/DataAccess/HomeDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using carousel_v1_mvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace carousel_v1_mvc.DataAccess
+{
+    public class HomeDataAccess
+    {
+        private static carousel_v1 db = new carousel_v1();
+
+        public static IEnumerable<Game> GetGames()
+        {
+            return db.Games.ToList();
+        }
+    }
+}
